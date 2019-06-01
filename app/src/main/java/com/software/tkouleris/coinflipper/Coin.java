108965fce6490d7a724f81e227b00670eaf0822d
darkheart101 <kurokokoro@windowslive.com>
2019-06-01 20:05:55 +0300

    refactor

---
+++ app/src/main/java/com/software/tkouleris/coinflipper/Coin.java
@@ -0,0 +1,60 @@
+package com.software.tkouleris.coinflipper;
+
+import android.content.Context;
+import android.content.res.Configuration;
+
+
+public class Coin {
+
+    private Context context;
+    private int screenSize = 0;
+
+    public Coin(Context current){
+
+        this.context = current;
+
+
+        screenSize = getScreenSize();
+    }
+
+    public int getScreenSize(){
+        int screenSize = context.getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        return screenSize;
+    }
+
+    public int get_HeadImageSizeResource(){
+        int imageResource;
+
+        switch(screenSize) {
+            case Configuration.SCREENLAYOUT_SIZE_NORMAL:
+                imageResource = R.drawable.head_normal;
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_SMALL:
+                imageResource = R.drawable.head_small;
+                break;
+            default:
+                imageResource = R.drawable.head_normal;
+        }
+
+        return imageResource;
+    }
+
+    public int get_TailsImageSizeResource(){
+        int imageResource;
+
+        switch(screenSize) {
+            case Configuration.SCREENLAYOUT_SIZE_NORMAL:
+                imageResource = R.drawable.tails_normal;
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_SMALL:
+                imageResource = R.drawable.tails_small;
+                break;
+            default:
+                imageResource = R.drawable.tails_normal;
+        }
+
+        return imageResource;
+    }
+}
--- app/src/main/java/com/software/tkouleris/coinflipper/MainActivity.java
+++ app/src/main/java/com/software/tkouleris/coinflipper/MainActivity.java
@@ -1,12 +1,11 @@
 package com.software.tkouleris.coinflipper;
 
-import android.content.res.Configuration;
+import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,100 +13,60 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Context context = this;
+        final Coin coin = new Coin(context );
+
         // Init everything
-        initialize();
+        initialize(coin);
 
         final ImageView image = findViewById(R.id.img_coin);
+
 
         image.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
 
-                flipTheCoin(image);
+                int imageHeadResource = coin.get_HeadImageSizeResource();
+                int imageTailsResource = coin.get_TailsImageSizeResource();
+
+                double tailsOrHead = flipTheCoin();
+
+                String msg = "";
+                if( (int)tailsOrHead == CoinSide.HEAD ) {
+                    image.setImageResource(imageHeadResource);
+                    msg = "Κορώνα!";
+                }
+
+                if( (int)tailsOrHead == CoinSide.TAILS ) {
+                    image.setImageResource(imageTailsResource);
+                    msg = "Γράμματα!";
+                }
+
+                displayMessage(msg);
             }
         });
     }
 
-    private void initialize(){
-        int screenSize = 0;
+    private void initialize(Coin coin){
         int imageResource;
         displayMessage("Πατήστε το νόμισμα για να γυρίσει...");
-        screenSize = getScreenSize();
-        imageResource = get_HeadImageSizeResource(screenSize);
+        imageResource = coin.get_HeadImageSizeResource();
 
         ImageView image = findViewById(R.id.img_coin);
         image.setImageResource(imageResource);
     }
 
-    private int get_HeadImageSizeResource(int screenSize){
-        int imageResource;
-
-        switch(screenSize) {
-            case Configuration.SCREENLAYOUT_SIZE_NORMAL:
-                imageResource = R.drawable.head_normal;
-                break;
-            case Configuration.SCREENLAYOUT_SIZE_SMALL:
-                imageResource = R.drawable.head_small;
-                break;
-            default:
-                imageResource = R.drawable.head_normal;
-        }
-
-        return imageResource;
-    }
-
-    private int get_TailsImageSizeResource(int screenSize){
-        int imageResource;
-
-        switch(screenSize) {
-            case Configuration.SCREENLAYOUT_SIZE_NORMAL:
-                imageResource = R.drawable.tails_normal;
-                break;
-            case Configuration.SCREENLAYOUT_SIZE_SMALL:
-                imageResource = R.drawable.tails_small;
-                break;
-            default:
-                imageResource = R.drawable.tails_normal;
-        }
-
-        return imageResource;
-    }
-
-    private int getScreenSize(){
-        int screenSize = getResources().getConfiguration().screenLayout &
-                Configuration.SCREENLAYOUT_SIZE_MASK;
-
-        return screenSize;
-    }
-
-    private void flipTheCoin(ImageView image){
+    private double flipTheCoin(){
         double tailsOrHead = 0.0;
-        int screenSize = 0;
-        int imageHeadResource = 0;
-        int imageTailsResource = 0;
-        String msg = "";
-
-        screenSize = getScreenSize();
-        imageHeadResource = get_HeadImageSizeResource(screenSize);
-        imageTailsResource = get_TailsImageSizeResource(screenSize);
 
         tailsOrHead = RandomNumberBetween(1,2);
 
-        if( (int)tailsOrHead == CoinSide.HEAD ) {
-            image.setImageResource(imageHeadResource);
-            msg = "Κορώνα!";
-        }
-
-        if( (int)tailsOrHead == CoinSide.TAILS ) {
-            image.setImageResource(imageTailsResource);
-            msg = "Γράμματα!";
-        }
-
-        displayMessage(msg);
+        return tailsOrHead;
     }
 
     private double RandomNumberBetween(int min, int max){
